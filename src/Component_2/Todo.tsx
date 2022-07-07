fec2de654f7ad5254d395d8c9090fbb377ea4007
shibanshL <shibansh.w@gmail.com>
2022-07-07 11:40:15 +0530

    Changed the group elements from todo to grids, For better alignment purposes

--- src/Component_2/Todo.tsx
+++ src/Component_2/Todo.tsx
@@ -48,7 +48,8 @@ export default function Todo({ todo, toggleComplete, handleDelete, newToggleComp
     
    return (
     <>
-        <Grid style={{}} grow>
+
+        {/* <Grid style={{}} grow>
             <Grid.Col className="todo" span={12}>
               {!todo.completed?
                 <Group direction="column" p='10px' align='apart' m='10px' style={{background:'rgba(255,255,255,0.5)',borderRadius:'60px', justifyContent:'space-between'}} grow>
@@ -75,6 +76,78 @@ export default function Todo({ todo, toggleComplete, handleDelete, newToggleComp
                             <GrClose id="i" style={{cursor:'pointer'}} onClick={() => handleDelete(todo.id)}/>
                         </Group>
                     </Group>
+                </Group>
+              :  
+              <Group direction="column" p='10px' align='apart' m='10px' style={{display:'none',background:'rgba(255,255,255,0.5)',borderRadius:'60px', justifyContent:'space-between'}} grow>
+                    <Group direction="row" p='5px'  style={{}} grow>
+                        <Group direction="row" style={{}} position='left'>
+                          <Checkbox onChange={() => toggleComplete(todo)}/>
+                          {num%2==0?<Text weight={600} color={'green'} style={{}} size="md">{todo.title}</Text>:<Input
+                              variant="filled"
+                              radius='xl'
+                              type="text"
+                              value={todo.title === "" ? newTitle : todo.title}
+                              className="list"
+                              onChange={handleChange}
+                          />}
+                        </Group>
+                        <Group style={{}} position="right" direction="row">
+                            {num%2==0?<AiTwotoneEdit id="i" style={{cursor:'pointer'}} onClick={() =>{ 
+                              handleEdit(todo, newTitle) 
+                              Input_Tag()
+                              }}/>:<MdAddCircleOutline id="i" style={{cursor:'pointer'}} onClick={() =>{ 
+                                handleEdit(todo, newTitle) 
+                                Input_Tag()
+                                }}/>}
+                            <GrClose id="i" style={{cursor:'pointer'}} onClick={() => handleDelete(todo.id)}/>
+                        </Group>
+                    </Group>
+                </Group>
+              }
+        </Grid.Col>
+      </Grid>
+      </> */}
+
+
+      <Grid style={{}} grow>
+            <Grid.Col className="todo" style={{}} span={12}>
+              {!todo.completed?
+                <Group direction="column" p='10px' align='apart' position="center" m='10px' style={{background:'rgba(255,255,255,0.5)',borderRadius:'60px',}} grow>
+                    <Grid dir="row" align='center' justify='center' p='5px'  style={{}} grow>
+                        <Grid.Col dir="row" span={10} style={{}}>
+                          <Grid grow>
+                            <Grid.Col span={2}>
+                               <Checkbox checked={false} onChange={() => {toggleComplete(todo)}}/>
+                            </Grid.Col>
+                            <Grid.Col span={10}>
+                                {num%2==0?<Text weight={600} color={'green'} style={{}} size="md">{todo.title}</Text>:<Input
+                                  variant="filled"
+                                  radius='xl'
+                                  type="text"
+                                  value={todo.title === "" ? newTitle : todo.title}
+                                  className="list"
+                                  onChange={handleChange}
+                              />}
+                            </Grid.Col>
+                          </Grid>
+                        </Grid.Col>
+                        <Grid.Col style={{background:'pink'}} span={2} dir="row">
+                          <Grid>
+                            <Grid.Col span={6}>
+                                {num%2==0?<AiTwotoneEdit id="i" style={{cursor:'pointer'}} onClick={() =>{ 
+                                  handleEdit(todo, newTitle) 
+                                  Input_Tag()
+                                  }}/>:<MdAddCircleOutline id="i" style={{cursor:'pointer'}} onClick={() =>{ 
+                                    handleEdit(todo, newTitle) 
+                                    Input_Tag()
+                                    }}/>}
+                              </Grid.Col>
+                              <Grid.Col span={6}>
+                                <GrClose id="i" style={{cursor:'pointer'}} onClick={() => handleDelete(todo.id)}/>
+                              </Grid.Col>
+                            </Grid>
+                        </Grid.Col>
+                    </Grid>
                 </Group>
               :  
               <Group direction="column" p='10px' align='apart' m='10px' style={{display:'none',background:'rgba(255,255,255,0.5)',borderRadius:'60px', justifyContent:'space-between'}} grow>
